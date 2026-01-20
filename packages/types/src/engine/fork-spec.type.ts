62569e9d62cdd734788c080cc7b3ecfa46d3dd6f
Andrew Mott <andrew@andrewmott.com>
2026-01-20 17:40:26 -0500

    feat(types): add initial ForkSpec type

--- packages/types/src/engine/fork-spec.type.ts
+++ packages/types/src/engine/fork-spec.type.ts
@@ -1,1 +1,23 @@
-export type ForkSpec = {};
+type OutputHash = string;
+type StepId = string;
+
+/**
+ * Spec for deriving run behavior.
+ * Currently has properties more specific to a Simulation Spec, in terms
+ * of execution details, but for now we are just modeling a specific case
+ * when forking an existing run + simulation in one type.
+ */
+export type ForkSpec = {
+  parentRunId: string;
+  flowDefMode: FlowDefMode;
+  forceRerunSteps: string[];
+  cascade: boolean;
+  stepOutputOverrides?: Record<StepId, OutputHash>;
+};
+
+export type FlowDefMode =
+  | { mode: "inherit" }
+  | {
+      mode: "override";
+      hash: string;
+    };
